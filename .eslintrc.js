01310d5a59f668b1aca6e1b199e79b3cf969f489
tomhave97 <mirko.tomhave@web.de>
2022-02-09 21:45:01 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:vue/vue3-strongly-recommended',
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        "@typescript-eslint"
+    ]
+}
